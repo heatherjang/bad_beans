e69f6a190b6fd781c5ba72c1da8912005d655fe0
heatherjang <heather.m.jang@gmail.com>
2015-03-03 03:06:20 +0000

    Generated users controller and users/new view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
+  validates :email, :firstname, :lastname,
+    presence: true
+
+  validates :password,
+    length: { in: 6..20 }, on: :create
+
 end
