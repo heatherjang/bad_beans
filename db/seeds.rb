f030f8039c21a7d72041ffd95fe865ffb40263f1
heatherjang <heather.m.jang@gmail.com>
2015-03-03 02:05:53 +0000

    Created and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create!(
+  title: "Monsters, Inc.",
+  director: "Pete Docter",
+  runtime_in_minutes: 92,
+  description: "Monsters generate their city's power by scaring children, but they are terribly afraid themselves of being contaminated by children, so when one enters Monstropolis, top scarer Sulley finds his world disrupted.",
+  release_date: "2002-02-08",
+  poster_image_url: "http://concertposter.org/-2013Mar/Monsters-Inc/Monsters-Inc-All-TeaserNT-drop.jpg"
+)
