88478718baf4f2870d519725f00c1a743d5e2014
heatherjang <heather.m.jang@gmail.com>
2015-03-03 22:57:58 +0000

    Created admins, restricts acces to regular users

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ RottenMangoes::Application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
+  namespace :admin do
+    resources :users
+  end
+
   root to: 'movies#index'
 
 
 end
+
