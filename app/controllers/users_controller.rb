abd71e718c5691f9ff03767df7d3f0a8e0cbea3a
heatherjang <heather.m.jang@gmail.com>
2015-03-04 19:46:27 +0000

    Admin can delete users and associated movie reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
   protected
 
   def user_params
