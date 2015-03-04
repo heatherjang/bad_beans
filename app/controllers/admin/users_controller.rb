c4f4158ec4cafe79754fbf3d9ec3b14250a66ef4
heatherjang <heather.m.jang@gmail.com>
2015-03-04 19:18:14 +0000

    Admins can create new users and set as admins

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,15 @@
 class Admin::UsersController < ApplicationController
 
   before_filter :restrict_access
-  before_filter :admin? 
+  before_filter :admin?
 
   def index
     @users = User.all.page(params[:page]).per(2)
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
   end
+
 end
